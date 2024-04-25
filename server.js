f408607d065b3d5a9787391fc86b17af7271c8e0
Rios marilyne <rios.marilyne@gmail.com>
2024-04-25 20:56:49 +0200

    test config cors youtube

--- server.js
+++ server.js
@@ -23,10 +23,11 @@ const port = process.env.PORT || 3001;
 
 app.use(
   cors({
-
+    origin: ['http://localhost:3000', 'https://localhost:3000'],
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   }),
 );
 
